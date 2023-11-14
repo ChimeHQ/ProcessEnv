cc3501d09f5bf38b74f45c5952540b72bf6a0de3
Matt <85322+mattmassicotte@users.noreply.github.com>
2023-11-14 13:17:40 -0500

    Update package def

--- Package.swift
+++ Package.swift
@@ -1,17 +1,26 @@
-// swift-tools-version:5.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.8
 
 import PackageDescription
 
 let package = Package(
-    name: "ProcessEnv",
-    platforms: [.macOS(.v10_11)],
-    products: [
-        .library(name: "ProcessEnv", targets: ["ProcessEnv"]),
-    ],
-    dependencies: [],
-    targets: [
-        .target(name: "ProcessEnv", dependencies: []),
-        .testTarget(name: "ProcessEnvTests", dependencies: ["ProcessEnv"]),
-    ]
+	name: "ProcessEnv",
+	platforms: [.macOS(.v10_13)],
+	products: [
+		.library(name: "ProcessEnv", targets: ["ProcessEnv"]),
+	],
+	dependencies: [],
+	targets: [
+		.target(name: "ProcessEnv", dependencies: []),
+		.testTarget(name: "ProcessEnvTests", dependencies: ["ProcessEnv"]),
+	]
 )
+
+let swiftSettings: [SwiftSetting] = [
+	.enableExperimentalFeature("StrictConcurrency")
+]
+
+for target in package.targets {
+	var settings = target.swiftSettings ?? []
+	settings.append(contentsOf: swiftSettings)
+	target.swiftSettings = settings
+}
